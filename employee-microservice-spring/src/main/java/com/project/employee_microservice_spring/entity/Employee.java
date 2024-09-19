177d67bd628a99dff5a9d1c8f3d68aad10072fcc
ashley <ashnand2000@gmail.com>
2024-09-19 16:16:45 +0530

    Microservices-7: Entity and Repository creation for EmployeeService

---
+++ employee-microservice-spring/src/main/java/com/project/employee_microservice_spring/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.project.employee_microservice_spring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String departmentCode;
+
+    private String organizationCode;
+
+}
