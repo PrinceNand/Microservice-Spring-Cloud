14f21085b745ef828162a7a7e494feaedf5fc870
ashley <ashnand2000@gmail.com>
2024-09-19 15:38:08 +0530

    Microservices-3: Entity and Repository creation for DepartmentService

---
+++ department-microservice-spring/src/main/java/com/project/department_microservice_spring/entity/Department.java
@@ -0,0 +1,22 @@
+package com.project.department_microservice_spring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+
+}
---
+++ department-microservice-spring/src/main/java/com/project/department_microservice_spring/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.project.department_microservice_spring.repository;
+
+import com.project.department_microservice_spring.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
