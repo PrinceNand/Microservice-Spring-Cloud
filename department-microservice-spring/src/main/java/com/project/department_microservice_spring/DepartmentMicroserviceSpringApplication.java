2d106ab1e281c673e5e6a302a1d607196c7b3ddd
ashley <ashnand2000@gmail.com>
2024-09-20 21:21:50 +0530

    Microservices-22: Eureka Server(Create and run multiple instance of service in another port - DepartmentService)

--- department-microservice-spring/src/main/java/com/project/department_microservice_spring/DepartmentMicroserviceSpringApplication.java
+++ department-microservice-spring/src/main/java/com/project/department_microservice_spring/DepartmentMicroserviceSpringApplication.java
@@ -10,4 +10,8 @@ public class DepartmentMicroserviceSpringApplication {
 		SpringApplication.run(DepartmentMicroserviceSpringApplication.class, args);
 	}
 
+	// for multiple instance using jar maven>lifecycle>package and type command in terminal
+	// cd department-microservice-spring
+	//java -jar -Dserver.port={enterNewPortNumber} target/-jarName-
+	//java -jar -Dserver.port=8085 target/department-microservice-spring-0.0.1-SNAPSHOT.jar
 }
