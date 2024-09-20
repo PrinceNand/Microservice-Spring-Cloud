e9e02b85c13d04c2ae9b163b7774f8c6b42b69df
ashley <ashnand2000@gmail.com>
2024-09-20 21:27:51 +0530

    Microservices-23: Eureka Server(Load balancer with Eureka Change FeignClient name in ApiClient to dynamically add all the instance URL)

--- employee-microservice-spring/src/main/java/com/project/employee_microservice_spring/service/ApiClient.java
+++ employee-microservice-spring/src/main/java/com/project/employee_microservice_spring/service/ApiClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "department-microservice-spring")
 public interface ApiClient {
 
     // Build get department rest api
