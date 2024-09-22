1fb7a09b2a9435bf6dad80d824f3c40385ccfdb9
ashley <ashnand2000@gmail.com>
2024-09-22 12:44:58 +0530

    Microservices-28: Config-Server(Create a new Config Server Service)

---
+++ config-server-cloud/src/test/java/com/project/config_server_cloud/ConfigServerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.config_server_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
