09ef6a4c9f9e7cfe81c32ac870a2ca27f4daf98a
ashley <ashnand2000@gmail.com>
2024-09-23 17:59:46 +0530

    Microservices-39: Config-Server-Bus(Creating a sample Rest controller for Employee Service similar to department service)

---
+++ employee-microservice-spring/src/main/java/com/project/employee_microservice_spring/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.project.employee_microservice_spring.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+    // add "spring.boot.message= Hello department Service!" in git cloud repo - department
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("user/message")
+    public String message() {
+        return message;
+
+    }
+
+}
